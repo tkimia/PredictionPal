7590476458e966a021bb55c2533e42a8ff32a591
zsoehren <zsoehren@purdue.edu>
2015-02-20 17:03:26 -0500

    Added match names and team names instead of undiscript autogenerated ones.

--- grails-app/domain/predictionpal/Match.groovy
+++ grails-app/domain/predictionpal/Match.groovy
@@ -8,7 +8,30 @@ class Match {
 	static mappedBy = [ prevMatches: "nextMatch"] //prevmatches on this obj defined by next on other obj
 
 	Team winner
-
+	
+	String toString() {
+		String result = "";
+		int i = 0;
+		for (Team T in teams){
+			if(i != 0){
+				result = result + " vs "
+			}
+			result = result + T
+			i++
+		}
+		i = 1;
+		if (result.equals("")){
+			def prevList = prevMatches.toArray()
+			while(prevList.size() > 0){
+				Match prev = prevList[0]
+				i++
+				prevList=prev.prevMatches.toArray()
+			}
+			result = "Round " + i + " empty match"
+		}
+		return result;
+	}
+	
     static constraints = {
     	nextMatch nullable: true
 		winner nullable: true
